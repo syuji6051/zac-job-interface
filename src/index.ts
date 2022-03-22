d2ce6b3d578719b2d5def050781fabaacb75873f
syuji <syuji.okamura@gmail.com>
2022-03-22 18:27:45 +0900

    add secrets

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as works from './works';
 export * as zac from './zac';
 export * as slack from './slack';
+export * as secrets from './secrets';
---
+++ src/secrets.ts
@@ -0,0 +1,14 @@
+export interface SecretsValues {
+  COGNITO_USER_POOL: string
+  ENCRYPT_KEY: string
+  ENCRYPT_SALT_KEY: string
+  PUPPETEER_API_URL: string
+  PUPPETEER_API_ACCESS_KEY: string
+  PUPPETEER_API_SECRET_KEY: string
+  SLACK_TOKEN: string
+  SLACK_CLIENT_ID: string
+  SLACK_CLIENT_SECRET: string
+  SLACK_REDIRECT_URL: string
+  SLACK_VERIFICATION_TOKEN: string
+  SNS_SLACK_MESSAGE_TOPIC: string
+};
